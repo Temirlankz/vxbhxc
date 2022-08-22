0a0de6f4edf38e4e2c06ef455f17d19414178a1e
Temirlankz <107919869+Temirlankz@users.noreply.github.com>
2022-08-22 21:37:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// var num = 10
+// let num2 = 12
+// const num3 = 14
+
+// function myFunc (){
+//     let num = 10;
+//     num++;
+//     console.log(num);
+//     return num;
+// }
+// let myVar = myFunc();
+// console.log(myVar)
+
+// let myPrompt = prompt()
+// // console.log(myPrompt);
+// alert(myPrompt)
+//1
+// let myPrompt1 = prompt()
+// let result1 = myPrompt1 ** 2
+
+// alert(result1)
+//2
+// let myPrompt2 = prompt()
+// let myPrompt20 = prompt()
+// let result2 = (+myPrompt2 + +myPrompt20) / 2;
+// alert(result2)
+//3
+// let myPrompt3 = prompt()
+// let result3 = myPrompt3 ** 2
+// alert(result3)
+//4
+// let myPrompt4 = prompt()
+// let result4 = myPrompt4 / 0.62
+// alert(result4)
+//5
+// let myPrompt5 = prompt()
+// let myPrompt50 = prompt()
+// let result5 = (myPrompt50 * -1) / myPrompt5
+// alert(result5)
+//6
+// let myPrompt7 = prompt("введите трех значное число")
+// let result7= myPrompt7 % 10
+// let result70= myPrompt7 - result7
+// let result700= result70 /10
+// let result7000 = result %10
+// alert(result7000)
+//7
+// let myPrompt8 = prompt()
+// let result8= myPrompt8 & 10
+// let result9=((+result8 * 100000 + +myPrompt8) - +result8) / 10
+// alert(result9)
+//8
+let a = 250
+let myPrompt10 = prompt()
+let result11= (+myPrompt10 * a) + 10% 
+alert(result11)
